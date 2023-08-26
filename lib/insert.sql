ccf48739602e911fc57c5b0a0e4c3158000bab87
mike <mjowiti@gmail.com>
2023-08-26 17:08:57 +0300

    done lab

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears(name,age,sex,color,temparament,alive)
+VALUES
+('Mr. Chocolate',2,'M','yellow','calm',1),
+('Rowdy',3,'F','white','brush',1),
+('Tabitha',8,'F','pink','calm',1),
+('Sergeant Brown',10,'M','black','happy',1),
+('Melissa',12,'F','chocolate','chilled',1),
+('Grinch',7,'M','yellow','chilled',1),
+('Wendy',2,'F','black','petty',1),
+(NULL,2,'M','yellow','calm',1);
+
+
+
